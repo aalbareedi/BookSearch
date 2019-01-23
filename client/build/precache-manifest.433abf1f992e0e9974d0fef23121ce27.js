104ff16fdfa57c16915c913cb9243702f5204f31
Amer Albareedi <aalbareedi@yahoo.com>
2019-01-23 13:30:41 -0600

    add mongoose and client/build static folder line

---
+++ client/build/precache-manifest.433abf1f992e0e9974d0fef23121ce27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5969cadc8f39a7077302",
+    "url": "/static/js/main.5969cadc.chunk.js"
+  },
+  {
+    "revision": "eb503d079ebf2301c4ab",
+    "url": "/static/js/1.eb503d07.chunk.js"
+  },
+  {
+    "revision": "5969cadc8f39a7077302",
+    "url": "/static/css/main.f16aefc8.chunk.css"
+  },
+  {
+    "revision": "ec32416742719b8dbe623d53db27f2eb",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 const app = express();
 //const apiRoutes = require("./routes/apiRoutes");
 const routes = require("./routes");
 
+const mongoose = require("mongoose");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+app.use(express.static("client/build"));
 
 var mongoURL = process.env.MONGODB_URI || "mongodb://localhost/article_db";
 
@@ -26,7 +28,7 @@ app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("/tom", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
